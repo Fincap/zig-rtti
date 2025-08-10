9b4f8dc5627cfe3eeafc9d960486661dd31a6180
Matthew Archer <matthew.e.archer9@gmail.com>
2025-08-10 12:12:48 +1000

    Added executable step to `build.zig` and `main` to `root.zig` as a development aid

--- build.zig
+++ build.zig
@@ -21,15 +21,23 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(lib);
 
-    // Check step
-    const lib_check = b.addLibrary(.{
-        .linkage = .static,
+    // Run step (for dev testing)
+    const exe = b.addExecutable(.{
         .name = project_name,
         .root_module = lib_mod,
     });
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 
+    // Check step
+    const exe_check = b.addExecutable(.{
+        .name = project_name,
+        .root_module = lib_mod,
+    });
     const check = b.step("check", "Check if " ++ project_name ++ " compiles");
-    check.dependOn(&lib_check.step);
+    check.dependOn(&exe_check.step);
 
     // Unit testing
     const lib_unit_tests = b.addTest(.{
--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub const fmt = @import("fmt.zig");
 pub const type_info = @import("type_info.zig");
@@ -10,6 +11,30 @@ pub const RTTIError = error{
     InvalidField,
     FormatError,
 };
+
+const TestUnion = union(enum) {
+    a: u8,
+    b: u16,
+    c: u32,
+    d: u32,
+    e: void,
+};
+
+fn printEnumInfo(comptime T: type) void {
+    const info = @typeInfo(T).@"union";
+    std.debug.print("---- {} ({?}) ----\n", .{ T, info.tag_type });
+    std.debug.print("  size: {d}, alignment: {d}\n", .{ @sizeOf(T), @alignOf(T) });
+    std.debug.print("  layout: {}\n", .{info.layout});
+    std.debug.print("  fields:\n", .{});
+    inline for (info.fields) |field| {
+        std.debug.print("    {s} ({?}), size: {d}, alignment: {d}\n", .{ field.name, field.type, @sizeOf(field.type), field.alignment });
+    }
+}
+
+pub fn main() !void {
+    std.debug.print("Optimize mode: {}\n", .{builtin.mode});
+    printEnumInfo(TestUnion);
+}
 
 test {
     std.testing.refAllDecls(@This());

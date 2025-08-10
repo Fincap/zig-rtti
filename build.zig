1306c3cec6068c5310d81f99cefca0874c5b7856
Matthew Archer <matthew.e.archer9@gmail.com>
2025-08-10 21:16:18 +1000

    Added `docs` step to `build.zig`

--- build.zig
+++ build.zig
@@ -3,6 +3,11 @@ const std = @import("std");
 const project_name: []const u8 = "rtti";
 
 pub fn build(b: *std.Build) void {
+    const test_step = b.step("test", "Run unit tests");
+    const run_step = b.step("run", "Run the app");
+    const check_step = b.step("check", "Check if " ++ project_name ++ " compiles");
+    const docs_step = b.step("docs", "Emit documentation");
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -17,8 +22,8 @@ pub fn build(b: *std.Build) void {
         .linkage = .static,
         .name = project_name,
         .root_module = lib_mod,
+        .use_llvm = true,
     });
-
     b.installArtifact(lib);
 
     // Run step (for dev testing)
@@ -28,7 +33,6 @@ pub fn build(b: *std.Build) void {
     });
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
-    const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
     // Check step
@@ -36,16 +40,21 @@ pub fn build(b: *std.Build) void {
         .name = project_name,
         .root_module = lib_mod,
     });
-    const check = b.step("check", "Check if " ++ project_name ++ " compiles");
-    check.dependOn(&exe_check.step);
+    check_step.dependOn(&exe_check.step);
 
     // Unit testing
     const lib_unit_tests = b.addTest(.{
         .root_module = lib_mod,
     });
-
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+
+    // Documentation
+    const docs_install = b.addInstallDirectory(.{
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+        .source_dir = lib.getEmittedLlvmIr(),
+    });
+    docs_step.dependOn(&docs_install.step);
+    b.getInstallStep().dependOn(docs_step);
 }

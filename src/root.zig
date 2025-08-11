b14fe16036c9fe9b057f5be11c5c48dd6de62057
Matthew Archer <matthew.e.archer9@gmail.com>
2025-08-11 13:10:28 +1000

    Added library `Options`, with option to choose `getTypeId` behaviour

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,23 @@
-pub const fmt = @import("fmt.zig");
 pub const Type = @import("type_info.zig").Type;
+pub const TypeRegistry = @import("TypeRegistry.zig");
+
 const type_id = @import("type_id.zig");
 pub const getTypeId = type_id.getTypeId;
 pub const TypeId = type_id.TypeId;
-pub const TypeRegistry = @import("TypeRegistry.zig");
+
+pub const fmt = @import("fmt.zig");
 pub const util = @import("util.zig");
+
+const root = @import("root");
+
+/// Library-wide options that can be overridden by the root file.
+pub const options: Options = if (@hasDecl(root, "rtti_options")) root.rtti_options else .{};
+
+pub const Options = struct {
+    /// Enables type ID generation via incrementing values in custom linker sections. If disabled,
+    /// then type IDs will be non-contiguous and non-sequential.
+    enable_linksection_typeid: bool = true,
+};
 
 test {
     @import("std").testing.refAllDecls(@This());
--- src/type_id.zig
+++ src/type_id.zig
@@ -1,17 +1,38 @@
+const options = @import("root.zig").options;
+
 pub const TypeId = usize;
 
 /// Returns a unique identifier for the given type, which can be used at runtime.
 ///
 /// Uses linksection to assign each type a unique offset-based ID at compile time, on the
 /// assumption that section `.bss.RTTI_Types0` will appear before `.bss.RTTI_Types1` in memory,
-/// however this behaviour is *not* guaranteed by the linker and may result in sparse IDs being
-/// generated.
+/// however this behaviour is *not* guaranteed by the linker and may result in non-contiguous and/or
+/// non-sequential IDs being generated.
+///
+/// If this behaviour is undesirable for your specific toolchain/target and you would prefer to
+/// explicitly generate non-contiguous and non-sequential IDs, you can configure the library in your
+/// project root:
+///
+/// ```
+/// pub const rtti_options: rtti.Options = .{
+///     .enable_linksection_typeid = false,
+/// };
+/// ```
+/// (This works the same way as `std.Options`)
 pub fn getTypeId(comptime T: type) TypeId {
-    const H = struct {
-        const byte: u8 linksection(section_name ++ "1") = 0;
-        const _ = T;
-    };
-    return &H.byte - &@"RTTI_Types.head";
+    if (options.enable_linksection_typeid) {
+        const H = struct {
+            const byte: u8 linksection(section_name ++ "1") = 0;
+            const _ = T;
+        };
+        return &H.byte - &@"RTTI_Types.head";
+    } else {
+        const H = struct {
+            const byte: u8 = 0;
+            const _ = T;
+        };
+        return @intFromPtr(&H.byte);
+    }
 }
 const section_name = ".bss.RTTI_Types";
 const @"RTTI_Types.head": u8 linksection(section_name ++ "0") = 0;
